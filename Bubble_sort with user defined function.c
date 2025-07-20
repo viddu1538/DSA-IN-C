6190d825c1205c1c7b24c8318862dbd012aa3c6e
vidya sagar <sagarvidya2956@gmail.com>
2025-07-20 20:43:14 +0530

    this is the bubble sorting algorithm for sorting.

---
+++ Bubble_sort with user defined function.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void Bubble_sort(int a[],int range)
+{
+	int k,temp,i;
+	for(k=0;k<range;k++)  // outer loop 
+	{
+		for(i=0;i<range-1;i++)  //inner loop 
+	    {
+		    if(a[i]>a[i+1])
+		    {
+			temp=a[i];
+			a[i]=a[i+1];
+			a[i+1]=temp;
+		    }
+	    }
+	}
+	printf("after sorting  the array is \n");
+	for(i=0;i<range;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
+int main(){  
+	int i,k,range;  // k= outer loop , i= inner loop , range = user inout size
+	printf("enter the range :");
+	scanf("%d",&range);
+	int a[range];
+	printf("enter %d numbers :",range);
+	for(i=0;i<range;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	Bubble_sort(a,range);
+	
+}
