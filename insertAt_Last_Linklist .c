6c07bf226afecc5d697f93af20c84d95dbe5ec5e
vidya sagar <sagarvidya2956@gmail.com>
2025-07-28 11:14:55 +0530

    insert at last and traverse linklist

---
+++ insertAt_Last_Linklist .c
@@ -0,0 +1,68 @@
+/** this is a linklist program in which we take user input in linklist and then 
+print/travere the linklist   and then we insert a node at start */
+#include<stdio.h>
+#include<stdlib.h>  // for malloc and calloc function 
+typedef struct node  // this node has two member 1st is int type and second is self refrential 
+{
+int data;
+struct node  *next;  // self refrential structure 
+}node;
+
+node * createNode()
+{
+	node *head,*q,*r;
+	char ch;
+	head=(node *)malloc(sizeof(node));
+	printf("enter data:");
+	scanf("%d",&head->data);
+	head->next=NULL;
+	q=head;
+	printf("first node created succesfully.\n");
+	printf("want more node ?(y/n)");
+	scanf(" %c",&ch);
+	while(ch=='y'||ch=='Y')
+	{
+		r=(node*)malloc(sizeof(node));
+		printf("enter data:");
+		scanf("%d",&r->data);
+		r->next=NULL;
+		q->next=r;
+		q=r; // to point new last node   , q is for 
+		printf("want more node ?(y/n)");
+    	scanf(" %c",&ch);
+	}
+	return head;	
+}
+void traverse(node *head)
+{
+	node *h; // for temporary loop 
+	for(h=head;h!=NULL;h=h->next)
+	   printf("%5d",h->data);
+}
+ 
+ 
+node *insert_last(node *head)
+{
+	node *z,*n;  // z is for loop and n is for new node at last 
+	n=(node *)malloc(sizeof(node));
+	printf("\n enter data for the new node :");
+	scanf("%d",&n->data);
+	n->next=NULL; // since it will be the last node so it hold null in next part
+	z=head; // initilazation of z is important if not then the loop start from random memory address 
+	while(z->next!=NULL)
+	{
+		z=z->next;    // traversing to the last node 
+	}
+	z->next=n;  // attatch new node 
+	return head;				
+}
+
+int main()
+{
+	node *h;
+	h=createNode();
+	traverse(h);
+	h=insert_last(h);
+	traverse(h);
+	
+}
